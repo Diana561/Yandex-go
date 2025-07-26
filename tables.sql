6834a352a3805b5f66eaa19e5017e58351046d84
Diana561 <DianaRakhimov@yandex.ru>
2025-07-26 13:31:07 +0500

    Add files via upload

---
+++ tables.sql
@@ -0,0 +1,15 @@
+-- public.newtable определение
+
+-- Drop table
+
+-- DROP TABLE public.newtable;
+
+CREATE TABLE public.newtable (
+	фио varchar NULL,
+	возраст int4 NULL,
+	"Номер телефона" int4 NULL,
+	"Заработная плата" int4 NULL,
+	"Кол-во смен в неделю" int4 NULL,
+	"Кол-во часов работы в день" varchar NULL,
+	должность varchar NULL
+);
